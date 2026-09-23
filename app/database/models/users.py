6f3a02771aa90465068586f0a079905608f2be8c
abdullahilyas3450 <abdullahilyas3450@gmail.com>
2026-09-23 19:44:24 +0500

    db models are setup and also the sceheme for tables are added

---
+++ app/database/models/users.py
@@ -0,0 +1,21 @@
+from .base import Base
+from datetime import datetime, timezone
+
+from sqlalchemy import DateTime, String, Text
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100))
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    description: Mapped[str | None] = mapped_column(Text, nullable=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+    )
